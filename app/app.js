e969ded1ada67d7f96880650bcf79dadcc6bca20
Puneet Oberoi <puneet.oberoi@outlook.com>
2016-09-20 11:59:51 -0400

    app.js rendering main

---
+++ app/app.js
@@ -0,0 +1,9 @@
+// Include Main React Dependencies
+var react = require('react');
+var reactDOM = require('react-dom');
+
+// Include Main Component
+var main = require('./components/main');
+
+// Render Main Component
+reactDOM.render(<main />, document.getElementById('app'));
